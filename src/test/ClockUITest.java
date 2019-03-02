9804dbb4372d45fe7bb3e00599dd8f6c16ae3df4
unknown <fff.naeimi@gmail.com>
2019-03-02 14:07:16 +0330

    clockLogicTest commit.

---
+++ src/test/ClockUITest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.clock.ui.ClockUI;
+
+public class ClockUITest {
+    public static void main(String[] args) {
+        ClockUI myClock = new ClockUI(0 , 0 , 0 );
+        myClock.setClock(2 , 30 , 45);
+    }
+}
