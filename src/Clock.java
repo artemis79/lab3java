8768f4c75d79aac8d23c8d3aaed4724d6b66d7b2
unknown <fff.naeimi@gmail.com>
2019-03-02 14:15:30 +0330

    clock class commit :) .

---
+++ src/Clock.java
@@ -0,0 +1,14 @@
+import org.clock.logic.ClockLogic;
+import org.clock.ui.ClockUI;
+import java.util.concurrent.TimeUnit;
+
+public class Clock {
+    public static void main(String[] args) {
+        ClockUI myClockUI = new ClockUI(0 , 0 , 0);
+        ClockLogic myClockLogic = new ClockLogic (0 , 0 , 0 );
+        for (int i = 0; ; ){
+            myClockLogic.tik(1);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
---
+++ src/org/clock/logic/ClockLogic.java
@@ -0,0 +1,36 @@
+package org.clock.logic;
+
+public class ClockLogic {
+    private int hour;
+    private  int minute;
+    private int second;
+    public ClockLogic(int hour, int minute, int second){
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+    public void setClock(int hour, int minute, int second){
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+    public void tik(int second){
+        this.second += second ;
+        this.minute += this.second / 60 ;
+        this.second = this.second %60 ;
+        this.hour += this.minute / 60;
+        this.minute = this.minute % 60;
+        this.hour = this.hour % 24;
+
+    }
+    public int getHour(){
+        return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond(){
+        return  second ;
+    }
+
+}
