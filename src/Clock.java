3dbeba421d01850e916215f13eaed0a44a05b438
unknown <fff.naeimi@gmail.com>
2019-03-02 14:19:50 +0330

    second clock class commit.

--- src/Clock.java
+++ src/Clock.java
@@ -8,6 +8,9 @@ public class Clock {
         ClockLogic myClockLogic = new ClockLogic (0 , 0 , 0 );
         for (int i = 0; ; ){
             myClockLogic.tik(1);
+            myClockUI.setHour(myClockLogic.getHour());
+            myClockUI.setMinute(myClockLogic.getMinute());
+            myClockUI.setSecond(myClockLogic.getSecond());
             TimeUnit.SECONDS.sleep(1);
         }
     }
